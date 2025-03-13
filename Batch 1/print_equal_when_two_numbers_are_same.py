c337f0a38c6be4fe79736468a2639c775986b5eb
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-13 22:09:15 +0800

    ask_user_for_inputs

--- Batch 1/print_equal_when_two_numbers_are_same.py
+++ Batch 1/print_equal_when_two_numbers_are_same.py
@@ -1,3 +1,6 @@
 # ask the user for inputs
+num_1 = float(input("Enter first number: "))
+num_2 = float(input("Enter second number: "))
+
 # if the numbers are equal, print "Equal"
 # else, print "Not equal"
