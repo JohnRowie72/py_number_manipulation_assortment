3f5a0b1699b9d6b88ee55bd26da6ff966dedc49a
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-13 22:43:11 +0800

    pseudocode

---
+++ Batch 1/quotient_with_decimal_point.py
@@ -0,0 +1,3 @@
+# ask user for two numbers
+# if the second number is not zero, compute the quotient of the two numbers then print the result with a decimal point
+# if the second number is zero, print an error message
