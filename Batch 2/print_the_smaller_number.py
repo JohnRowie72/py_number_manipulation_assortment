ddaa06ca9cff7c0f4709aa193a2fa1efd832a428
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-14 00:14:04 +0800

    ask_for_inputs_and_print_the_smaller_number

--- Batch 2/print_the_smaller_number.py
+++ Batch 2/print_the_smaller_number.py
@@ -1,3 +1,8 @@
 # ask the use for inputs
+num_1 = float(input("Enter the first number: "))
+num_2 = float(input("Enter the second number: "))
+
 # compare the two numbers
 # print the smaller number
+smaller_number_result = min(num_1, num_2)
+print("The smaller number is: ", smaller_number_result)
