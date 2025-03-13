d5b23db302aa027d13bc20a8cc988226d066f1ba
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-13 23:10:45 +0800

    ask_the_user_for_inputs_then_print_the_output

--- Batch 1/result_of_raised_by_a_number.py
+++ Batch 1/result_of_raised_by_a_number.py
@@ -1,3 +1,8 @@
 # ask the user for two numbers
+num_1 = int(input("Enter base number: "))
+num_2 = int(input("Enter exponent number: "))
+
 # compute the result of raising the first number to the power of the second number
 # print the result
+result = num_1 ** num_2
+print(f"The result of raising {num_1} to the power of {num_2} is {result}")
