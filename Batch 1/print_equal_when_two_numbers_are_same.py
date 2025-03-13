10b0d2bfd5ad656010e4522f5dddef7d3cc34b5c
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-13 22:14:11 +0800

    print_equal_when_the_two_numbers_are_the_same_else_print_not_equal

--- Batch 1/print_equal_when_two_numbers_are_same.py
+++ Batch 1/print_equal_when_two_numbers_are_same.py
@@ -3,4 +3,8 @@ num_1 = float(input("Enter first number: "))
 num_2 = float(input("Enter second number: "))
 
 # if the numbers are equal, print "Equal"
-# else, print "Not equal"
+# else, print "Not Equal"
+if num_1 == num_2:
+    print("Equal")
+else:
+    print("Not Equal")
