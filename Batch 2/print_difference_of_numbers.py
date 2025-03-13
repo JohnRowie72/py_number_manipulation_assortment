f9348ee45e68a21d984491cf94761be90ab60597
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-14 00:25:52 +0800

    ask_for_inputs_and_print_the_difference

--- Batch 2/print_difference_of_numbers.py
+++ Batch 2/print_difference_of_numbers.py
@@ -1,3 +1,9 @@
 # ask the user for inputs
+num_1 = float(input("Enter the first number: "))
+num_2 = float(input("Enter the second number: "))
+
 # subtract the two numbers
+difference_of_two_numbers = num_1 - num_2
+
 # print the difference of the two numbers
+print("The difference of the two numbers is: ", difference_of_two_numbers)
