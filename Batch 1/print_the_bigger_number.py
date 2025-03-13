b43790a915635c9193df28ab71a68fdcad02342e
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-13 21:38:28 +0800

    compare_the_two_numbers_and_print_the_bigger_number

--- Batch 1/print_the_bigger_number.py
+++ Batch 1/print_the_bigger_number.py
@@ -4,3 +4,5 @@ num_2 = float(input("Enter second number: "))
 
 # compare the two numbers
 # print the bigger number
+bigger_number_result = max(num_1, num_2)
+print("The bigger number is: ", bigger_number_result)
