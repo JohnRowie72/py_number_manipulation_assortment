4b2cb09ce844ef4bc3c1c3771755c7dbe2aab1b3
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-13 23:54:30 +0800

    print_all_numbers_except_ending_0

--- Batch 1/print_numbers_except_ending_0.py
+++ Batch 1/print_numbers_except_ending_0.py
@@ -1,3 +1,10 @@
 # print all the numbers from 0 to 100
-# check if the number is ending with 0
-# print if true
+for number in range(0, 101):
+
+    # check if the number is ending with 0
+    if number % 10 != 0:
+
+        # print if true
+        print(number, end=" ")
+
+print()
