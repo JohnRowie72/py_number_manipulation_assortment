c6197ede4905e832748204d5a3a053cd2a79d67b
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-13 23:43:07 +0800

    print_all_even_numbers

--- Batch 1/even_numbers_starting_from_0_to_100.py
+++ Batch 1/even_numbers_starting_from_0_to_100.py
@@ -1,3 +1,8 @@
 # loop from 0 to 100
-# check if the number is even
-# print only the even numbers
+for i in range(0, 101, 2):
+    # check if the number is even
+    if i % 2 == 0:
+        # print only the even numbers
+        print(i, end=" ")
+
+print()
