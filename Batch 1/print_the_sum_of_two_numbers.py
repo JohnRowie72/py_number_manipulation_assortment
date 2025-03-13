53c359a56ce10f4d5cb9049a5569878c8b2eb1ca
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-13 22:26:03 +0800

    ask_user_for_inputs_and_print_the_sum_of_two_numbers

--- Batch 1/print_the_sum_of_two_numbers.py
+++ Batch 1/print_the_sum_of_two_numbers.py
@@ -1,3 +1,8 @@
 # ask the user for inputs
+num_1 = float(input("Enter first number: "))
+num_2 = float(input("Enter second number: "))
+
 # add the two numbers
 # print the sum of the two numbers
+sum_of_two_numbers = num_1 + num_2
+print("The sum of the two numbers is:", sum_of_two_numbers)
