663d1fdf74787a87b069d1a4189032eefb4d6643
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-13 22:34:35 +0800

    ask_user_for_inputs_and_print_the_product_of_the_two_numbers

--- Batch 1/print_the_product_of_two_numbers.py
+++ Batch 1/print_the_product_of_two_numbers.py
@@ -1,3 +1,8 @@
 # ask user for inputs
+num_1 = float(input("Enter first number: "))
+num_2 = float(input("Enter second number: "))
+
 # multiply the two numbers
 # print the product of the two numbers
+product_of_two_numbers = num_1 * num_2
+print("The product of the two numbers is:", product_of_two_numbers)
