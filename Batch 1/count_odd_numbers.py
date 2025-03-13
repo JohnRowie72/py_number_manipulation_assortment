5a9e7a662f6a89de05fbe3a1f2833139fed8e3ff
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-13 23:32:07 +0800

    ask_for_10_inputs_then_print_how_many_odd_numbers

--- Batch 1/count_odd_numbers.py
+++ Batch 1/count_odd_numbers.py
@@ -1,4 +1,12 @@
 # ask the user for 10 numbers
+count_of_odd_numbers = 0
+
 # check if each number is odd
 # count how many numbers are odd
+for i in range(10):
+    num = float(input(f"Enter a number {i+1}: "))
+    if num % 2 != 0:
+        count_of_odd_numbers += 1
+
 # print the count of odd numbers
+print(f"The count of odd numbers is: {count_of_odd_numbers}")
