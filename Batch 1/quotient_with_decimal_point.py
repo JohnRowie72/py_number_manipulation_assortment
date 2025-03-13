195cabf50de37904cc88b8c8f0ee00e0b8af49cc
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-13 22:46:02 +0800

    ask_for_inputs_then_print_the_quotient_with_decimal_point

--- Batch 1/quotient_with_decimal_point.py
+++ Batch 1/quotient_with_decimal_point.py
@@ -1,3 +1,11 @@
 # ask user for two numbers
+num_1 = float(input("Enter first number: "))
+num_2 = float(input("Enter second number: "))
+
 # if the second number is not zero, compute the quotient of the two numbers then print the result with a decimal point
 # if the second number is zero, print an error message
+if num_2 != 0:
+    quotient_of_two_numbers = num_1 / num_2
+    print("The quotient of the two numbers is:", quotient_of_two_numbers)
+else:
+    print("Error: Cannot divide by zero")
