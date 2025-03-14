c82b452732fd4e216adcf17384ec990caec508ec
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-14 16:38:47 +0800

    add_input_loop_and_list_storage

--- Batch 4/display_numbers_from_highest_lowest.py
+++ Batch 4/display_numbers_from_highest_lowest.py
@@ -1,6 +1,13 @@
 # initialize an empty list to store the numbers
+numbers = []
+
 # start a loop to take input continuously
+while True:
     # try to convert input to an integer
+    try:
+        number = int(input("Enter a number: "))
+        numbers.append(number)
+        
         # sort the list in descending
         # display the sorted list
     # if input is invalid (ValueError)
