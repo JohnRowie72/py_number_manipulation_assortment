8706dc064dac414298b4d82c2895793e82487f67
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-14 16:46:59 +0800

    sort_and_display_list_in_descending_order_and_handling_invalid_input

--- Batch 4/display_numbers_from_highest_lowest.py
+++ Batch 4/display_numbers_from_highest_lowest.py
@@ -7,9 +7,15 @@ while True:
     try:
         number = int(input("Enter a number: "))
         numbers.append(number)
-        
+
         # sort the list in descending
+        numbers.sort(reverse=True)
+
         # display the sorted list
+        print(f"The numbers from highest to lowest: {numbers}")
+
     # if input is invalid (ValueError)
+    except ValueError:
         # print error message
-        # break the loop
+        print("Invalid input. Stopping the program")
+        break
