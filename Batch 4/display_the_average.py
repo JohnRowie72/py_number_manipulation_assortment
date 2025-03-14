0d7c761fe419f7e927617425cab330483aa6ec34
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-14 16:53:57 +0800

    add_input_loop_and_list_storage

--- Batch 4/display_the_average.py
+++ Batch 4/display_the_average.py
@@ -1,3 +1,14 @@
 # initialize an empty list to store the numbers
+numbers = []
+
 # start a loop to take input continuously
+while True:
+    try:
+        number = int(input("Enter a number: "))
+        numbers.append(number)
+
+    except ValueError:
+        print("Invalid input. Stopping the program")
+        break
+    
 # display the result
