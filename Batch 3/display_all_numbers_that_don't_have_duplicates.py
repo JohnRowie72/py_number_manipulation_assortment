724c76003b93d6780d2d87983ab137247c492903
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-14 11:43:35 +0800

    add_input_and_list_storage_for_10_numbers

--- Batch 3/display_all_numbers_that_don't_have_duplicates.py
+++ Batch 3/display_all_numbers_that_don't_have_duplicates.py
@@ -1,5 +1,11 @@
 # ask the user to input 10 numbers
+numbers = []
+
 # store the numbers in a list
+for i in range(10):
+    number = int(input(f"Enter a number {i+1}: "))
+    numbers.append(number)
+    
 # check for unique numbers
 # if the number appears only once, add to unique list
 # display all unique numbers
