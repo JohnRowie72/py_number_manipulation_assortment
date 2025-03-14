5948294ae11b3f748f6949d981119c60ab9d4a1f
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-14 11:56:29 +0800

    unique_number_filtering_and_display

--- Batch 3/display_all_numbers_that_don't_have_duplicates.py
+++ Batch 3/display_all_numbers_that_don't_have_duplicates.py
@@ -1,11 +1,17 @@
 # ask the user to input 10 numbers
 numbers = []
+unique_numbers = []
 
 # store the numbers in a list
 for i in range(10):
     number = int(input(f"Enter a number {i+1}: "))
     numbers.append(number)
-    
+
 # check for unique numbers
 # if the number appears only once, add to unique list
+for number in numbers:
+    if numbers.count(number) == 1:
+        unique_numbers.append(number)
+
 # display all unique numbers
+print(f"The unique numbers are: {unique_numbers}")
