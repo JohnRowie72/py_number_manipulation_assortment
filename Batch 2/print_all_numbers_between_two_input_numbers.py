b643c7064357e6852874f20866e82938a3bc612a
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-14 09:26:15 +0800

    print_the_output

--- Batch 2/print_all_numbers_between_two_input_numbers.py
+++ Batch 2/print_all_numbers_between_two_input_numbers.py
@@ -3,4 +3,11 @@ num_1 = int(input("Enter the first number: "))
 num_2 = int(input("Enter the second number: "))
 
 # determine the smaller number and the larger number
+smaller_num = min(num_1, num_2)
+larger_num = max(num_1, num_2)
+
 # print all the numbers between the smaller number and the larger number
+for i in range(smaller_num + 1, larger_num):
+    print(i, end=" ")
+
+print()
