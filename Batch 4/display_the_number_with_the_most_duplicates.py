15e19d0f7747b7e4cfc3b9a19c43f7bf8346c6b0
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-14 15:58:08 +0800

    pseudocode

---
+++ Batch 4/display_the_number_with_the_most_duplicates.py
@@ -0,0 +1,9 @@
+# initialize an empty list to store the numbers
+# start a loop to ask for input continuously
+    # try to convert input to an integer
+        # add to the list
+        # find the number with the most duplicates
+        # display the most frequent number
+    # if input is invalid (ValueError)
+        # print error message
+        # break the loop
