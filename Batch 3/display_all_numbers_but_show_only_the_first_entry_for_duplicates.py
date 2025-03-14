d9f42ce91b7e67642b3ad4429f5f18c593ccbfc3
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-14 12:17:00 +0800

    input_handling_and_list_storage_for_10_numbers

--- Batch 3/display_all_numbers_but_show_only_the_first_entry_for_duplicates.py
+++ Batch 3/display_all_numbers_but_show_only_the_first_entry_for_duplicates.py
@@ -1,3 +1,12 @@
 # ask the user to input 10 numbers
+numbers = []
+number_seen = set()
+
 # store numbers and check for duplicates (keep only the first entry)
+for i in range(10):
+    number = int(input(f"Enter a number {i+1}: "))
+    if number not in number_seen:
+        numbers.append(number)
+        number_seen.add(number)
+
 # display the result
