ddfd5f924d88e09e29b237ae01db20534c9e191a
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-14 14:47:49 +0800

    pseudocode

---
+++ Batch 3/display_sort_the_numbers_lowest_highest.py
@@ -0,0 +1,9 @@
+# initialize an empty list to store the numbers
+# start a loop to take user input continuously
+    # try to convert input to an integer
+        # add the number to the list
+        # sort the list in ascending order
+        #dDisplay the sorted list
+    # if input is invalid (ValueError)
+        # print error message
+        # break the loop
