9fd7208ef158a45bbfaed4be534a03dc2643e454
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-14 08:01:23 +0800

    pseudocode

---
+++ Batch 2/print_quotient_without_decimal_point.py
@@ -0,0 +1,4 @@
+# ask the user for two numbers
+# if the second number is not zero, compute the integer division
+# print the quotient without the decimal point
+# if the second number is zero, print an error message
