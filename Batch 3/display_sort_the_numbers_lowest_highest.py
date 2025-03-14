794de61732804bd17f1b457c9ecfd24b8d57836f
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-14 20:18:07 +0800

    fixed_display_result_only_after_user_input_ends

--- Batch 3/display_sort_the_numbers_lowest_highest.py
+++ Batch 3/display_sort_the_numbers_lowest_highest.py
@@ -6,17 +6,18 @@ while True:
     try:
         # try to convert input to an integer
         number = int(input("Enter a number: "))
-
         # add the number to the list
         numbers.append(number)
 
-        # to sort the list in ascending order
-        numbers.sort()
-        
-        # display the sorted list
-        print(f"The numbers in order: {numbers}")
-
-    # if input is invalid (ValueError)
     except ValueError:
         print("Invalid input. Stopping the program")
         break
+
+# display and to sort the list in ascending order
+if numbers:
+    numbers.sort()
+    # display the sorted list
+    print(f"The numbers from lowest to highest: {numbers}")
+
+else:
+    print("No numbers were entered")
