54e064b9c699c0218765f1792376ae82c96ef11f
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-14 12:40:43 +0800

    fixed_pseudocode_not_showing

---
+++ Batch 3/show_unique_or_duplicate.py
@@ -0,0 +1,5 @@
+# create a list to store input numbers
+# keep asking for input until an invalid entry is made
+    # ask the user to input a number
+    # check if the number is already in the list
+    # if input is invalid, stop asking for input
