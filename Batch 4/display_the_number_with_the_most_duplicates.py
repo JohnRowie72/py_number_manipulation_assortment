501413c21c4b6c183cf423a8939ac8d44b8ce298
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-14 16:09:44 +0800

    implement_logic_to_find_the_number_with_the_most_duplicates

--- Batch 4/display_the_number_with_the_most_duplicates.py
+++ Batch 4/display_the_number_with_the_most_duplicates.py
@@ -8,9 +8,13 @@ while True:
     try:
         number = int(input("Enter a number: "))
         numbers.append(num)
-        
+
         # find the number with the most duplicates
+        most_frequent_number = max(set(numbers), key=numbers.count)
+
         # display the most frequent number
+        print(f"The number with the most duplicates is: {most_frequent_number}")
+        
     # if input is invalid (ValueError)
         # print error message
         # break the loop
