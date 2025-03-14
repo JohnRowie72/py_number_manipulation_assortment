d516f9d25d3b41ea0f32aa5275d971894d66863d
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-14 08:11:37 +0800

    ask_the_user_for_two_numbers_and_print_the_remainder

--- Batch 2/print_the_remainder.py
+++ Batch 2/print_the_remainder.py
@@ -1,4 +1,12 @@
 # ask the user for two numbers
+num_1 = int(input("Enter the first number: "))
+num_2 = int(input("Enter the second number: "))
+
 # if the second number is not zero, compute the remainder
 # print the remainder
+if num_2 != 0:
+    print(f"The remainder of {num_1} and {num_2} is {num_1 % num_2}")
+
 # if the second number is zero, print an error message
+else:
+    print("Error: Division by zero is not allowed.")
