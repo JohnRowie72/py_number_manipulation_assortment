d501ff527f9dc923cb69a76978afa35675c1e3bc
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-14 16:18:46 +0800

    pseudocode

---
+++ Batch 4/display_the_highest_number.py
@@ -0,0 +1,5 @@
+# initialize an empty list to store the numbers
+# start a loop to take input continuously
+    # try to convert input to an integer
+        # display the highest number so far using max()
+    # If input is invalid (ValueError)
