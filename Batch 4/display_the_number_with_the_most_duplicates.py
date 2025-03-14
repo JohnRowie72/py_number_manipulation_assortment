476a1914468654b554c0eafe1a6a1e2769d5bca9
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-14 16:04:05 +0800

    add_input_loop_and_list_storage

--- Batch 4/display_the_number_with_the_most_duplicates.py
+++ Batch 4/display_the_number_with_the_most_duplicates.py
@@ -1,7 +1,14 @@
 # initialize an empty list to store the numbers
+numbers = []
+
 # start a loop to ask for input continuously
+while True:
+
     # try to convert input to an integer
-        # add to the list
+    try:
+        number = int(input("Enter a number: "))
+        numbers.append(num)
+        
         # find the number with the most duplicates
         # display the most frequent number
     # if input is invalid (ValueError)
