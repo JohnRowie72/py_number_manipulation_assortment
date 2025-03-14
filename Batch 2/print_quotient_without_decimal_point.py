0ed0cc8bee6dd2d540aca4b7ea75650c2109cebb
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-14 08:05:32 +0800

    ask_for_inputs_and_print_the_output

--- Batch 2/print_quotient_without_decimal_point.py
+++ Batch 2/print_quotient_without_decimal_point.py
@@ -1,4 +1,12 @@
 # ask the user for two numbers
+num_1 = int(input("Enter the first number: "))
+num_2 = int(input("Enter the second number: "))
+
 # if the second number is not zero, compute the integer division
 # print the quotient without the decimal point
+if num_2 != 0:
+    print(f"The quotient of {num_1} and {num_2} without decimal is {num_1 // num_2}")
+
 # if the second number is zero, print an error message
+else:
+    print("Error: Division by zero is not allowed.")
