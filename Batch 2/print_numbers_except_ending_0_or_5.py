9d05f0270db4e866e701cb8d82593a8f11d50dbb
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-14 09:11:51 +0800

    fixed_number_range_and_added_print_output

--- Batch 2/print_numbers_except_ending_0_or_5.py
+++ Batch 2/print_numbers_except_ending_0_or_5.py
@@ -1,7 +1,11 @@
 # print all number 0 - 100
 
 for number in range(0, 101):
-    print(number)
    
-# check if ending with 0 or 5
-    # print if true
+    # check if ending with 0 or 5
+    if number % 10 != 0 and number % 10 != 5:
+
+        # print if true
+        print(number, end=" ")
+
+print()
