7dd8a90530697af006993365a17c9ae05e9412c6
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-14 08:48:00 +0800

    print_the_output

--- Batch 2/print_all_odd_numbers_from_0_to_100.py
+++ Batch 2/print_all_odd_numbers_from_0_to_100.py
@@ -1,4 +1,11 @@
 # start from 1
+number = 1
+
 # while number is less than or equal to 100
 # if number is odd, print the number
 # increment number by 2
+while number <= 100:
+    print(number, end=" ")
+    number += 2
+
+print()
