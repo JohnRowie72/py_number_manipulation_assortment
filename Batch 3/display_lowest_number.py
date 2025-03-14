b6cf0d563f0a3d0e1d679cd4075f7b8122f1910f
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-14 13:31:35 +0800

    input_handling_list_storage_and_logic_to_display_lowest_number

--- Batch 3/display_lowest_number.py
+++ Batch 3/display_lowest_number.py
@@ -1,5 +1,14 @@
 # create a list to store inputs
+numbers = []
+
 # keep askibf for input until an invalid entry is made
-    # ask the user to input a number
-    # display the result
+while True:
+    try:
+        # ask the user to input a number
+        number = int(input("Enter a number: "))
+        numbers.append(number)
+        
+        # display the result
+        print(f"The lowest number is: {min(numbers)}")
+        
     # if input is invalid, stop asking for input
