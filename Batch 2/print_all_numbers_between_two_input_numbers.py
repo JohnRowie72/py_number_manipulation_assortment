891264e9c2e5ab1c0826aa92650a38b5dfed816b
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-14 09:23:37 +0800

    ask_for_two_inputs

--- Batch 2/print_all_numbers_between_two_input_numbers.py
+++ Batch 2/print_all_numbers_between_two_input_numbers.py
@@ -1,3 +1,6 @@
 # ask the user for two numbers
+num_1 = int(input("Enter the first number: "))
+num_2 = int(input("Enter the second number: "))
+
 # determine the smaller number and the larger number
 # print all the numbers between the smaller number and the larger number
