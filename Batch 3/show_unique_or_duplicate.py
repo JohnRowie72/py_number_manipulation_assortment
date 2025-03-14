061da56ff07a17c127fd0e0eaf66d5e68d446d62
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-14 12:50:38 +0800

    loop_input_and_checking_for_unique_and_duplicate_numbers

--- Batch 3/show_unique_or_duplicate.py
+++ Batch 3/show_unique_or_duplicate.py
@@ -1,5 +1,18 @@
 # create a list to store input numbers
+numbers = []
+
 # keep asking for input until an invalid entry is made
-    # ask the user to input a number
-    # check if the number is already in the list
-    # if input is invalid, stop asking for input
+while True:
+    try:
+        # ask the user to input a number
+        number = int(input("Enter a number: "))
+        
+        # check if the number is already in the list
+        if number in numbers:
+            print(f"{number} is a duplicate.")
+        else:
+            print(f"{number} is unique.")
+            numbers.append(number)
+            
+    except ValueError:
+        break
