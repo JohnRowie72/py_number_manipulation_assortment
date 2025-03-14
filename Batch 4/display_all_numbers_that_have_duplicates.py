8bf44a2b4e8a5082acb83e7c3884ba6c277d61cd
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-14 15:29:09 +0800

    pseudocode

---
+++ Batch 4/display_all_numbers_that_have_duplicates.py
@@ -0,0 +1,7 @@
+# initialize an empty list to store the numbers
+# ask the user for 10 numbers and store them in the list
+# initialize an empty list to store duplicate numbers
+# loop through the list
+    # if a number appears more than once and is not yet in the duplicates list
+        # add it to the duplicates list
+# display the duplicate numbers
