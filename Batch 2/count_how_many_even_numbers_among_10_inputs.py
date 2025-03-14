482e2e0d69e3d9db3087e8f385b7e0bb5ae23ccd
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-14 08:37:20 +0800

    ask_the_user_for_inputs_and_print_the_result

--- Batch 2/count_how_many_even_numbers_among_10_inputs.py
+++ Batch 2/count_how_many_even_numbers_among_10_inputs.py
@@ -1,4 +1,12 @@
 # ask the user to input 10 numbers
+count_even = 0
+
 # check if each number is even
 # count how many numbers are even
+for i in range(10):
+    number = int(input(f"Enter a number {i+1}: "))
+    if number % 2 == 0:
+        count_even += 1
+
 # print the count of even numbers
+print(f"The count of even numbers is {count_even}")
