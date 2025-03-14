d12bf7dff07d86f3b41f72ad304f2d587345bd7b
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-14 19:52:15 +0800

    fix_display_result_only_after_user_input_ends

--- Batch 3/display_lowest_number.py
+++ Batch 3/display_lowest_number.py
@@ -7,11 +7,15 @@ while True:
         # ask the user to input a number
         number = int(input("Enter a number: "))
         numbers.append(number)
-        
-        # display the result
-        print(f"The lowest number entered is: {min(numbers)}")
 
     except ValueError:
         # if input is invalid, stop asking for input
         print("Invalid input. Stopping the program.")
+        break
 
+# display the lowest number
+if numbers:
+    print(f"The lowest number entered is: {min(numbers)}")
+
+else:
+    print("No numbers were entered")
