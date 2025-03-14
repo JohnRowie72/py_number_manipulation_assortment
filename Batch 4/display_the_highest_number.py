728ed8db0fd6c85e08e6491e2d91e6e763c1b36e
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-14 16:22:23 +0800

    add_input_loop_and_list_storage

--- Batch 4/display_the_highest_number.py
+++ Batch 4/display_the_highest_number.py
@@ -1,5 +1,12 @@
 # initialize an empty list to store the numbers
+numbers = []
+
 # start a loop to take input continuously
+while True:
     # try to convert input to an integer
+    try:
+        number = int(input("Enter a number: "))
+        numbers.append(number)
+        
         # display the highest number so far using max()
     # If input is invalid (ValueError)
