8872d79ee12c83076604f02106b52ee56e710d5c
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-14 08:29:30 +0800

    ask_the_user_for_10_numbers_and_print_the_result

--- Batch 2/first_number_minus_all_remaining_numbers.py
+++ Batch 2/first_number_minus_all_remaining_numbers.py
@@ -1,3 +1,11 @@
 # ask the user for 10 numbers
+num_1 = float(input("Enter the first number: "))
+sum_result = num_1
+
 # subtract the remaining 9 numbers from the first number
+for i in range(9):
+    number = float(input("Enter the next number: "))
+    sum_result -= number
+
 # print the result
+print(f"The result of subtracting all the numbers from the first number is {sum_result}")
