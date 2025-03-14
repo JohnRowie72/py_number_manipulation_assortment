122e0bf8959a41228d3d9f0112a37f729958bd89
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-14 13:27:14 +0800

    pseudocode

---
+++ Batch 3/display_lowest_number.py
@@ -0,0 +1,5 @@
+# create a list to store inputs
+# keep askibf for input until an invalid entry is made
+    # ask the user to input a number
+    # display the result
+    # if input is invalid, stop asking for input
