e02ecbcb1e846fc23678009ad16605c27453499b
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-14 15:40:29 +0800

    add-input_loop_and_list_storage_for_10_numbers

--- Batch 4/display_all_numbers_that_have_duplicates.py
+++ Batch 4/display_all_numbers_that_have_duplicates.py
@@ -1,5 +1,11 @@
 # initialize an empty list to store the numbers
+numbers = [] 
+
 # ask the user for 10 numbers and store them in the list
+for i in range(10):
+    number = int(input(f"Enter number {i+1}"))
+    numbers.append(number)
+    
 # initialize an empty list to store duplicate numbers
 # loop through the list
     # if a number appears more than once and is not yet in the duplicates list
